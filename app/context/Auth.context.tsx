7d85da9c83b2162c7dd928c13cedef3cae065384
MrAlao <ishomon@gmail.com>
2024-08-13 14:34:58 +0100

    Add custom auth provider

---
+++ app/context/Auth.context.tsx
@@ -0,0 +1,44 @@
+import { User } from "lucia";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+interface Response {
+  user: User | null;
+  isLoading: boolean;
+}
+
+export const AuthContext = createContext<Response | null>(null);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function checkSession() {
+      const res = await fetch("/api/authdata");
+      const data = await res.json();
+      setUser(data);
+      setIsLoading(false);
+    }
+
+    checkSession();
+  }, []);
+  return (
+    <AuthContext.Provider value={{ user, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within a AuthProvider");
+  }
+  return context;
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import NavBar from "./components/NavBar";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AuthProvider } from "./context/Auth.context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,8 +25,10 @@ export default function RootLayout({
       <body className={inter.className}>
         <GoogleOAuthProvider clientId={process.env.GOOGLE_OAUTH_CLIENT_ID!}>
           <MantineProvider>
-            <NavBar />
-            <main className="p-5 lg:p-0 lg:w-4/5 mx-auto">{children}</main>
+            <AuthProvider>
+              <NavBar />
+              <main className="p-5 lg:p-0 lg:w-4/5 mx-auto">{children}</main>
+            </AuthProvider>
             <ToastContainer theme="colored" />
           </MantineProvider>
         </GoogleOAuthProvider>
